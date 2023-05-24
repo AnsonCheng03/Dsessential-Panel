671230abd04c9e5ccf7090329aba5ddf1c0c06da
Anson Cheng <Anson.12666@icloud.com>
2023-05-24 13:19:04 +0800

    feat: integrate sql database

--- nest-js/src/auth/auth.service.ts
+++ nest-js/src/auth/auth.service.ts
@@ -1,3 +1,4 @@
+import { compare } from 'bcrypt';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
@@ -11,10 +12,15 @@ export class AuthService {
 
   async signIn(username, pass) {
     const user = await this.usersService.findOne(username);
-    if (user?.password !== pass) {
+
+    if (
+      !user ||
+      !await compare(pass, user.password)
+    ) {
       throw new UnauthorizedException();
     }
-    const payload = { sub: user.userId, username: user.username };
+
+    const payload = { sub: user.userId, username: user.userId };
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
--- nest-js/src/users/users.service.ts
+++ nest-js/src/users/users.service.ts
@@ -1,28 +1,49 @@
 import { Injectable } from '@nestjs/common';
+import { createPool } from 'mysql2/promise';
 
 // This should be a real class/interface representing a user entity
 export type User = {
+  role: string;
   userId: number;
-  username: string;
   password: string;
 };
 
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'test',
-      password: 'test',
-    },
-    {
-      userId: 2,
-      username: 'Anson',
-      password: 'Anson2003',
-    },
-  ];
+  private readonly pool = createPool({
+    host: process.env.DB_HOST || 'localhost',
+    port: parseInt(process.env.DB_PORT) || 3306,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_LOGIN,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+  });
 
   async findOne(username: string): Promise<User | undefined> {
-    return this.users.find(user => user.username === username);
-  }
+    const connection = await this.pool.getConnection();
+    try {
+      const [TotalAcc] = await connection.execute(
+        'SELECT `SID` as Username FROM`Student` \
+        WHERE ? IN(`SID`, `卡內號碼`, `卡號`, `補發卡卡號`, `姓名`, `學生電話`, `家長電話`) LIMIT 1',
+        [username]
+      );
+
+      const [rows] = await connection.execute(
+        'SELECT `Password` FROM `Login` WHERE `SID` = ? LIMIT 1'
+        , [TotalAcc[0].Username]);
+      
+      const user = {
+        role: 'user',
+        userId: TotalAcc[0].Username,
+        password: rows[0].Password.replace(/^\$2y(.+)$/i, '$2a$1'),
+      };
+      return user;
+    } catch (err) {
+      return undefined;
+    } finally {
+      connection.release();
+    }
+  } 
 }
