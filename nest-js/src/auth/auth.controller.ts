0f358f3c7160a9632a51ffb80f80e40fe58f65f6
Anson Cheng <Anson.12666@icloud.com>
2023-05-24 18:48:59 +0800

    feat: auth guard on endpoint

--- nest-js/src/auth/auth.controller.ts
+++ nest-js/src/auth/auth.controller.ts
@@ -1,14 +1,29 @@
-import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards
+} from '@nestjs/common';
+import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  // NOTE: endpoint for login: http://localhost:3000/auth/login
   @HttpCode(HttpStatus.OK)
   @Post('login')
-  signIn(@Body() signInDto: Record<string, string>) {
+  signIn(@Body() signInDto: Record<string, any>) {
     return this.authService.signIn(signInDto.role, signInDto.ID, signInDto.password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
