3af365e80e6e3e258242ca184ca229e2d7c08d20
Anson Cheng <Anson.12666@icloud.com>
2023-05-28 19:12:41 +0800

    feat: auth redirect when not authorized

--- qwik-app/src/routes/Dsessential/auth/index.tsx
+++ qwik-app/src/routes/Dsessential/auth/index.tsx
@@ -1,5 +1,10 @@
-import { component$, useSignal, $ } from "@builder.io/qwik";
-import { type DocumentHead, useNavigate, Form } from "@builder.io/qwik-city";
+import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
+import {
+  type DocumentHead,
+  useNavigate,
+  Form,
+  RequestEvent,
+} from "@builder.io/qwik-city";
 import { useAuthSession, useAuthSignin } from "~/routes/plugin@auth";
 import style from "./index.module.css";
 
@@ -9,75 +14,82 @@ export default component$(() => {
   const nav = useNavigate();
   const adminRole = useSignal(false);
 
-  return (
-    <div class={style.center}>
-      <div class={style.content}>
-        <h1 class={style.h1}>
-          尚研閱文憑試支援中心
-          <br />
-          登入分區
-        </h1>
-        <Form action={signIn}>
-          <input type="hidden" name="providerId" value="credentials" />
-          <div class={style.switchToggle}>
-            <input
-              class={style.input}
-              id="student"
-              name="options.role"
-              type="radio"
-              value="student"
-              checked={!adminRole.value}
-              onClick$={() => {
-                adminRole.value = false;
-              }}
-            />
-            <label class={style.label} for="student">
-              學生
-            </label>
-            <input
-              class={style.input}
-              id="admin"
-              name="options.role"
-              type="radio"
-              value="admin"
-              bind:checked={adminRole}
-            />
-            <label class={style.label} for="admin">
-              管理員
-            </label>
-          </div>
-          <input
-            class={[style.input, style.loginInput]}
-            placeholder={
-              adminRole.value ? "職員名稱" : "卡號/電話號碼/中文姓名"
-            }
-            name="options.username"
-            type="text"
-            autoComplete="username"
-          />
-          <input
-            class={[style.input, style.loginInput]}
-            placeholder="密碼"
-            name="options.password"
-            type="password"
-            autoComplete="current-password"
-          />
-          <br />
-          <button class={style.button}>登入</button>
-          <div
-            class={style.resetPasswordContainer}
-            onClick$={() => {
-              nav("auth/resetpw");
-            }}
-          >
-            <div class={style.resetPassword}>
-              按此重設密碼<a class={style.bold}>首次登入請留空密碼</a>
+  useVisibleTask$(async () => {
+    if (session.value) {
+      nav("/Dsessential");
+    }
+  });
+
+  if (!session.value)
+    return (
+      <div class={style.center}>
+        <div class={style.content}>
+          <h1 class={style.h1}>
+            尚研閱文憑試支援中心
+            <br />
+            登入分區
+          </h1>
+          <Form action={signIn}>
+            <input type="hidden" name="providerId" value="credentials" />
+            <div class={style.switchToggle}>
+              <input
+                class={style.input}
+                id="student"
+                name="options.role"
+                type="radio"
+                value="student"
+                checked={!adminRole.value}
+                onClick$={() => {
+                  adminRole.value = false;
+                }}
+              />
+              <label class={style.label} for="student">
+                學生
+              </label>
+              <input
+                class={style.input}
+                id="admin"
+                name="options.role"
+                type="radio"
+                value="admin"
+                bind:checked={adminRole}
+              />
+              <label class={style.label} for="admin">
+                管理員
+              </label>
             </div>
-          </div>
-        </Form>
+            <input
+              class={[style.input, style.loginInput]}
+              placeholder={
+                adminRole.value ? "職員名稱" : "卡號/電話號碼/中文姓名"
+              }
+              name="options.username"
+              type="text"
+              autoComplete="username"
+            />
+            <input
+              class={[style.input, style.loginInput]}
+              placeholder="密碼"
+              name="options.password"
+              type="password"
+              autoComplete="current-password"
+            />
+            <br />
+            <button class={style.button}>登入</button>
+            <div
+              class={style.resetPasswordContainer}
+              onClick$={() => {
+                nav("auth/resetpw");
+              }}
+            >
+              <div class={style.resetPassword}>
+                按此重設密碼<a class={style.bold}>首次登入請留空密碼</a>
+              </div>
+            </div>
+          </Form>
+        </div>
       </div>
-    </div>
-  );
+    );
 });
 
 export const head: DocumentHead = {
