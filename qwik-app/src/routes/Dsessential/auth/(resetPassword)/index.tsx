626178f5e8887ead47063d720fe6fe6ba9e22c9c
Anson Cheng <Anson.12666@icloud.com>
2023-06-04 19:49:21 +0800

    chore: refactor reset page

--- qwik-app/src/routes/Dsessential/auth/(resetPassword)/index.tsx
+++ qwik-app/src/routes/Dsessential/auth/(resetPassword)/index.tsx
@@ -1,5 +1,5 @@
-import { type Signal, component$, useVisibleTask$ } from "@builder.io/qwik";
-import { Form } from "@builder.io/qwik-city";
+import { type Signal, component$, useSignal } from "@builder.io/qwik";
+import LocateUser from "./(locateUser)";
 interface Props {
   formState: Signal<"signIn" | "resetPassword">;
   adminRole: Signal<boolean>;
@@ -10,20 +10,21 @@ interface Props {
 export default component$(
   ({ formState, adminRole, style, userName }: Props) => {
     /* Todo: Add token to prevent brute force attack */
-
-    useVisibleTask$(() => {
-      if (adminRole.value)
-        setTimeout(() => {
-          formState.value = "signIn";
-        }, 3000);
-    });
+    const resetState = useSignal<
+      "default" | "securityCheck" | "resetPassword" | "updatePassword"
+    >("default");
 
     if (adminRole.value) {
       return (
         <div class={style.content}>
           <p>Admin password reset is not implemented yet.</p>
           <p>Please contact the developer. </p>
-          <button onClick$={() => (formState.value = "signIn")}>Go Back</button>
+          <button
+            class={style.button}
+            onClick$={() => (formState.value = "signIn")}
+          >
+            Go Back
+          </button>
         </div>
       );
     }
@@ -31,57 +32,13 @@ export default component$(
     return (
       <div class={style.content}>
         <h1 class={style.h1}>重置密碼</h1>
-        <Form>
-          <div class={style.switchToggle}>
-            <input
-              class={style.input}
-              id="student"
-              name="options.role"
-              type="radio"
-              value="student"
-              checked={!adminRole.value}
-              onClick$={() => {
-                adminRole.value = false;
-              }}
-            />
-            <label class={style.label} for="student">
-              學生
-            </label>
-            <input
-              class={style.input}
-              id="admin"
-              name="options.role"
-              type="radio"
-              value="admin"
-              bind:checked={adminRole}
-            />
-            <label class={style.label} for="admin">
-              管理員
-            </label>
-          </div>
-          <br />
-          <input
-            class={[style.input, style.loginInput]}
-            placeholder={
-              adminRole.value ? "職員名稱" : "卡號/電話號碼/中文姓名"
-            }
-            name="options.username"
-            type="text"
-            autoComplete="username"
-            bind:value={userName}
-          />
-          <br />
-          <button
-            onClick$={() => {
-              formState.value = "signIn";
-            }}
-            class={style.button}
-          >
-            返回
-          </button>
-
-          <button class={style.button}>下一步</button>
-        </Form>
+        <LocateUser
+          formState={formState}
+          resetState={resetState}
+          style={style}
+          userName={userName}
+          adminRole={adminRole}
+        />
       </div>
     );
   }
