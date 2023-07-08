a24b89c4103eff010db10a189cc4bd53030ad82c
Anson Cheng <Anson.12666@icloud.com>
2023-07-08 10:33:22 +0800

    chore: make nav bar in component

---
+++ qwik-app/src/components/navBar/navBar.tsx
@@ -0,0 +1,146 @@
+import { type QRL, component$ } from "@builder.io/qwik";
+import styles from "./navBar.module.css";
+import { Link } from "@builder.io/qwik-city";
+import { useAuthSignout } from "~/routes/plugin@auth";
+
+export default component$(({ navlist }: { navlist: any }) => {
+  const baseURL = "/Dsessential";
+  const signOut = useAuthSignout();
+
+  return (
+    <nav class={styles.header}>
+      <div class={styles.container}>
+        <input type="checkbox" class={styles.mobileCheck} />
+
+        <div class={styles.logoContainer}>
+          <h3 class={styles.logo}>
+            <Link href={baseURL} class={styles.whiteLink}>
+              DSE<span>ssential</span>
+            </Link>
+          </h3>
+        </div>
+
+        <div class={styles.navButton}>
+          <div class={styles.navLinks}>
+            <ul class={styles.navList}>
+              {navlist.map((items: any) => {
+                return (
+                  <li class={styles.navLink} key={items[1] as string}>
+                    {items[0] == "Item" && (
+                      <Link
+                        href={
+                          ((items[2].toLowerCase().startsWith("http")
+                            ? ""
+                            : baseURL) + items[2]) as string
+                        }
+                      >
+                        {items[1]}
+                      </Link>
+                    )}
+                    {items[0] == "List" && (
+                      <>
+                        <Link
+                          href={
+                            ((items[2].toLowerCase().startsWith("http")
+                              ? ""
+                              : baseURL) + items[2]) as string
+                          }
+                        >
+                          {items[1]}
+                        </Link>
+                        <div class={styles.dropdown}>
+                          <ul class={styles.navList}>
+                            {items[3].map((items: any) => {
+                              return (
+                                <li
+                                  class={styles.dropdownLink}
+                                  key={items[1] as string}
+                                >
+                                  {items[0] == "Item" && (
+                                    <Link
+                                      href={
+                                        ((items[2]
+                                          .toLowerCase()
+                                          .startsWith("http")
+                                          ? ""
+                                          : baseURL) + items[2]) as string
+                                      }
+                                    >
+                                      {items[1]}
+                                    </Link>
+                                  )}
+                                  {items[0] == "List" && (
+                                    <>
+                                      <Link
+                                        href={
+                                          ((items[2]
+                                            .toLowerCase()
+                                            .startsWith("http")
+                                            ? ""
+                                            : baseURL) + items[2]) as string
+                                        }
+                                      >
+                                        {items[1]}
+                                      </Link>
+                                      <div
+                                        class={[styles.dropdown, styles.second]}
+                                      >
+                                        <ul class={styles.navList}>
+                                          {items[3]?.map((items: string[]) => {
+                                            return (
+                                              <li
+                                                class={styles.dropdownLink}
+                                                key={items[1]}
+                                              >
+                                                <Link
+                                                  href={
+                                                    (items[2]
+                                                      .toLowerCase()
+                                                      .indexOf("stream") === -1
+                                                      ? ""
+                                                      : baseURL) + items[2]
+                                                  }
+                                                >
+                                                  {items[1]}
+                                                </Link>
+                                              </li>
+                                            );
+                                          })}
+                                          <div class={styles.arrow} />
+                                        </ul>
+                                      </div>
+                                    </>
+                                  )}
+                                </li>
+                              );
+                            })}
+                            <div class={styles.arrow} />
+                          </ul>
+                        </div>
+                      </>
+                    )}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+
+          <div class={styles.logoutSign}>
+            <a
+              class={[styles.button, styles.transparent]}
+              onClick$={() => signOut.submit({})}
+            >
+              登出
+            </a>
+          </div>
+        </div>
+
+        <div class={styles.hamburgerMenuContainer}>
+          <div class={styles.hamburgerMenu}>
+            <div />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+});
