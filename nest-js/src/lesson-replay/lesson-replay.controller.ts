18f16dca86adb414205da38ec99173d2663b026e
Anson Cheng <Anson.12666@icloud.com>
2023-07-11 04:17:21 +0800

    feat: connect frontend fetch video to backend

--- nest-js/src/lesson-replay/lesson-replay.controller.ts
+++ nest-js/src/lesson-replay/lesson-replay.controller.ts
@@ -1,36 +1,23 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
-import { AuthService } from 'src/auth/auth.service';
+import {
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('lesson-replay')
 export class LessonReplayController {
   //   constructor(private authService: AuthService) {}
 
+  @UseGuards(AuthGuard)
   @HttpCode(HttpStatus.OK)
-  @Post('video')
-  //   http://localhost:3500/lesson-replay/video
-  signIn(@Body() signInDto: Record<string, any>) {
-    console.log('video');
+  @Post('videoList')
+  //   http://localhost:3500/lesson-replay/videoList
+  getVideo(@Request() req) {
+    console.log(req.user);
     return 'aaa';
   }
 }
-
-// @Controller('auth')
-// export class AuthController {
-//   constructor(private authService: AuthService) {}
-
-//   @HttpCode(HttpStatus.OK)
-//   @Post('login')
-//   signIn(@Body() signInDto: Record<string, any>) {
-//     return this.authService.signIn(
-//       signInDto.role,
-//       signInDto.username,
-//       signInDto.password,
-//     );
-//   }
-
-//   @UseGuards(AuthGuard)
-//   @Get('profile')
-//   getProfile(@Request() req) {
-//     return req.user;
-//   }
-// }
--- qwik-app/src/routes/Dsessential/panel/(student)/lessonReplay/index.tsx
+++ qwik-app/src/routes/Dsessential/panel/(student)/lessonReplay/index.tsx
@@ -1,8 +1,33 @@
 import { component$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
 import UsefulLinks from "./components/usefulLinks/usefulLinks";
 import Videos from "./components/videos/videos";
 
+export const getVideoList = routeLoader$(async (requestEvent) => {
+  const accessToken = requestEvent.sharedMap.get("session").accessToken;
+  try {
+    const res = await fetch(
+      `http://${requestEvent.url.hostname}:3500/lesson-replay/videoList`,
+      {
+        cache: "no-store",
+        method: "POST",
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+
+    const data = await res.json();
+    if (!data.statusCode) return data;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 export default component$(() => {
+  const videoList = getVideoList().value;
+  console.log(videoList);
+
   return (
     <>
       <Videos />
