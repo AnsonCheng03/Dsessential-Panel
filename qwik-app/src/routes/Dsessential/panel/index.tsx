84e2241a16519ae46032ecf454900f39bd80eb3d
Anson Cheng <Anson.12666@icloud.com>
2023-07-10 16:46:28 +0800

    chore: change student data fetch to server side

--- qwik-app/src/routes/Dsessential/panel/index.tsx
+++ qwik-app/src/routes/Dsessential/panel/index.tsx
@@ -1,19 +1,39 @@
 import { component$ } from "@builder.io/qwik";
-import { type DocumentHead } from "@builder.io/qwik-city";
-import { useAuthSession } from "~/routes/plugin@auth";
+import { routeLoader$, type DocumentHead } from "@builder.io/qwik-city";
+import { useAuthSession, useAuthSignout } from "~/routes/plugin@auth";
 import styles from "./index.module.css";
 import logo from "~/components/logo/logo.png";
 
+export const getUserData = routeLoader$(async (requestEvent) => {
+  const access_token = requestEvent.sharedMap.get("session").user.access_token;
+  try {
+    const res = await fetch(
+      `http://${requestEvent.url.hostname}:3500/auth/profile`,
+      {
+        cache: "no-store",
+        headers: {
+          authorization: `Bearer ${access_token}`,
+        },
+      }
+    );
+
+    const data = await res.json();
+    if (!data.statusCode) return data;
+    throw "Error";
+  } catch (error) {
+    throw requestEvent.redirect(302, `/api/auth/signout?error=${error}`);
+  }
+});
+
 export default component$(() => {
-  const session = useAuthSession();
-  const user = (session.value as any).user;
+  const userData = getUserData().value;
 
   return (
     <div class={styles.profileBox}>
       <img src={logo}></img>
       <br />
-      學生編號：{user.username} <br />
-      角色： {user.role === "student" ? "學生" : "管理員"}
+      學生編號：{userData?.username} <br />
+      角色： {userData?.role === "student" ? "學生" : "管理員"}
     </div>
   );
 });
