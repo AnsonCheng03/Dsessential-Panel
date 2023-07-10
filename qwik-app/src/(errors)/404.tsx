dcd4ec38913efd74632a01d1144515e777f65034
Anson Cheng <Anson.12666@icloud.com>
2023-07-10 15:24:34 +0800

    feat: add 404 page placeholder

---
+++ qwik-app/src/(errors)/404.tsx
@@ -0,0 +1,32 @@
+import { component$ } from "@builder.io/qwik";
+import { type DocumentHead } from "@builder.io/qwik-city";
+import styles from "./error.module.css";
+
+export default component$(() => {
+  return (
+    <main class={styles.main}>
+      <div style={styles.container}>
+        <h1>抱歉，好像有點問題！</h1>
+        <br />
+        <p>
+          Type: 404 / IP:
+          {
+            //   Todo: add remote IP
+          }
+        </p>
+        <p>如問題持續，請聯絡管理員（admin@dsessential.com）</p>
+      </div>
+      <div style={styles.bottom} />
+    </main>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "404 | 尚研閱文憑試支援中心",
+  meta: [
+    {
+      name: "description",
+      content: "尚研閱文憑試支援中心",
+    },
+  ],
+};
--- qwik-app/src/routes/index.tsx
+++ qwik-app/src/routes/index.tsx
@@ -1,15 +1,15 @@
 import { component$ } from "@builder.io/qwik";
 import { Link, type DocumentHead } from "@builder.io/qwik-city";
-import style from "./index.module.css";
+import styles from "./index.module.css";
 
 export default component$(() => {
   return (
-    <div class={style.center}>
+    <div class={styles.center}>
       <h1>尚研閱文憑試支援中心</h1>
       <h2>分區選擇</h2>
-      <div class={style.container}>
+      <div class={styles.container}>
         <Link
-          class={style.card}
+          class={styles.card}
           href="/"
           onClick$={() => {
             window.alert("暫時停用，請聯絡95575713預約房間~");
@@ -17,7 +17,7 @@ export default component$(() => {
         >
           預約房間
         </Link>
-        <Link class={style.card} href="/Dsessential">
+        <Link class={styles.card} href="/Dsessential">
           學生專區
         </Link>
       </div>
