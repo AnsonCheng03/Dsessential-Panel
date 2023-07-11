27ed0dabc1c52b7de2b63746e98f24f0c27413e7
Anson Cheng <Anson.12666@icloud.com>
2023-07-11 19:58:59 +0800

    fix: css on long text

--- qwik-app/src/routes/Dsessential/panel/(student)/lessonReplay/components/videos/videos.tsx
+++ qwik-app/src/routes/Dsessential/panel/(student)/lessonReplay/components/videos/videos.tsx
@@ -2,10 +2,30 @@ import { component$, useSignal } from "@builder.io/qwik";
 import Search from "./search/search";
 
 export default component$(({ videoList }: any) => {
-  const availableMonth = Object.entries(videoList).map(([key, value]) => [
-    key,
-    Object.keys(value as Object),
-  ]);
+  // const availableMonth = Object.entries(videoList).map(([key, value]) => [
+  //   key,
+  //   Object.keys(value as Object),
+  // ]);
+  const availableMonth = [
+    [
+      "2022",
+      [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ],
+    ],
+    ["2023", ["Jan"]],
+  ];
   const selectedYear = useSignal(availableMonth[0][0] as string);
   const selectedMonth = useSignal(availableMonth[0][1][0] as string);
 
