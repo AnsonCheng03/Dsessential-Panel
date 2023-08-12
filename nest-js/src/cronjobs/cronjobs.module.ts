f751811b5df4fa31e7148bb38f8dffffbb89c4b7
Anson Cheng <Anson.12666@icloud.com>
2023-08-13 01:13:54 +0800

    Feat profile admin (#11)
    * chore: remove cronjob entrance
    
    * chore: add alert
    
    * chore: integrate link box from old platform
    
    * fix: data source change to 1D array from 2D
    
    * chore: video log in desc order
    
    * chore:  video log UI

--- nest-js/src/cronjobs/cronjobs.module.ts
+++ nest-js/src/cronjobs/cronjobs.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { CronjobsService } from './cronjobs.service';
-import { CronjobsController } from './cronjobs.controller';
 
 @Module({
   providers: [CronjobsService],
-  controllers: [CronjobsController],
 })
 export class CronjobsModule {}
