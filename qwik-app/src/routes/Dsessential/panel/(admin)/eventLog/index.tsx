35a1146a5542b113954f76682f484b2c27f3e559
Anson Cheng <Anson.12666@icloud.com>
2023-11-16 05:34:59 +0800

    feat: Update eventLog UI & sorting functionality

--- qwik-app/src/routes/Dsessential/panel/(admin)/eventLog/index.tsx
+++ qwik-app/src/routes/Dsessential/panel/(admin)/eventLog/index.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { $, component$, useSignal, useStore } from "@builder.io/qwik";
 import styles from "./index.module.css";
 import { routeLoader$ } from "@builder.io/qwik-city";
 
@@ -29,18 +29,94 @@ export const useGetAllUser = routeLoader$(async (requestEvent) => {
 
 export default component$(() => {
   const options = useGetAllUser().value;
+  const displayOptions = useStore(options);
+  const currentSortOption = useSignal("Date");
+
+  const sortOption = $((option: string) => {
+    if (option === currentSortOption.value) {
+      displayOptions.reverse();
+      return;
+    }
+
+    switch (option) {
+      case "Date":
+        currentSortOption.value = "Date";
+        displayOptions.sort((a: any, b: any) => {
+          return new Date(b.Date).getTime() - new Date(a.Date).getTime();
+        });
+        break;
+      case "UserID":
+        currentSortOption.value = "UserID";
+        displayOptions.sort((a: any, b: any) => {
+          return a.UserID.localeCompare(b.UserID);
+        });
+        break;
+      case "Event":
+        currentSortOption.value = "Event";
+        displayOptions.sort((a: any, b: any) => {
+          return a.Event.localeCompare(b.Event);
+        });
+        break;
+      case "Notes":
+        currentSortOption.value = "Notes";
+        displayOptions.sort((a: any, b: any) => {
+          return a.Notes.localeCompare(b.Notes);
+        });
+        break;
+      default:
+        break;
+    }
+    console.log(displayOptions);
+  });
+
   return (
     <>
       <h1 class={styles.title}>系統紀錄</h1>
 
       <div class={styles.container}>
         <div class={styles.item}>
-          <div>日期</div>
-          <div>用戶名稱</div>
-          <div>事件</div>
-          <div>備註</div>
+          <div
+            onClick$={() => {
+              sortOption("Date");
+            }}
+          >
+            日期
+            {currentSortOption.value === "Date" && (
+              <span class={styles.arrow}> •</span>
+            )}
+          </div>
+          <div
+            onClick$={() => {
+              sortOption("UserID");
+            }}
+          >
+            用戶名稱
+            {currentSortOption.value === "UserID" && (
+              <span class={styles.arrow}> •</span>
+            )}
+          </div>
+          <div
+            onClick$={() => {
+              sortOption("Event");
+            }}
+          >
+            事件
+            {currentSortOption.value === "Event" && (
+              <span class={styles.arrow}> •</span>
+            )}
+          </div>
+          <div
+            onClick$={() => {
+              sortOption("Notes");
+            }}
+          >
+            備註
+            {currentSortOption.value === "Notes" && (
+              <span class={styles.arrow}> •</span>
+            )}
+          </div>
         </div>
-        {options.map((option: any, index: number) => {
+        {displayOptions.map((option: any, index: number) => {
           return (
             <div key={index} class={styles.item}>
               <div>
