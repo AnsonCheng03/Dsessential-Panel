16d13763fbcbc637580c48321468ece2c00584fe
Anson Cheng <Anson.12666@icloud.com>
2024-06-13 02:26:16 +0800

    debug

--- qwik-app/src/createProxyOptions.tsx
+++ qwik-app/src/createProxyOptions.tsx
@@ -12,65 +12,65 @@ export const createProxyOptions = (
   preserveHeaderKeyCase: true,
   pathRewrite: pathRewriteFn,
   secure: false,
-  plugins: [
-    (proxyServer: any) => {
-      proxyServer.on(
-        "proxyReq",
-        (proxyReq: http.ClientRequest, req: express.Request) => {
-          // Check if cookie has specified token, if not, drop the request
-          if (cookieCheckToken) {
-            if (
-              !req.headers.cookie ||
-              !req.headers.cookie.includes(cookieCheckToken)
-            ) {
-              console.warn(
-                "No session token found in request, url requesting:",
-                req.url
-              );
-              proxyReq.destroy();
-              return;
-            }
-          }
+  // plugins: [
+  //   (proxyServer: any) => {
+  //     proxyServer.on(
+  //       "proxyReq",
+  //       (proxyReq: http.ClientRequest, req: express.Request) => {
+  //         // Check if cookie has specified token, if not, drop the request
+  //         if (cookieCheckToken) {
+  //           if (
+  //             !req.headers.cookie ||
+  //             !req.headers.cookie.includes(cookieCheckToken)
+  //           ) {
+  //             console.warn(
+  //               "No session token found in request, url requesting:",
+  //               req.url
+  //             );
+  //             proxyReq.destroy();
+  //             return;
+  //           }
+  //         }
 
-          if (req.body) {
-            const contentType = req.get("content-type");
-            const contentLength = req.get("content-length");
-            if (contentType) proxyReq.setHeader("content-type", contentType);
-            if (contentLength) {
-              const bodyData = JSON.stringify(req.body);
-              const bufferLength = Buffer.byteLength(bodyData);
-              if (bufferLength != parseInt(contentLength)) {
-                console.warn(
-                  `buffer length = ${bufferLength}, content length = ${contentLength}`
-                );
-                proxyReq.setHeader("content-length", bufferLength);
-              }
-              proxyReq.write(bodyData);
-            }
-          }
-        }
-      );
+  //         if (req.body) {
+  //           const contentType = req.get("content-type");
+  //           const contentLength = req.get("content-length");
+  //           if (contentType) proxyReq.setHeader("content-type", contentType);
+  //           if (contentLength) {
+  //             const bodyData = JSON.stringify(req.body);
+  //             const bufferLength = Buffer.byteLength(bodyData);
+  //             if (bufferLength != parseInt(contentLength)) {
+  //               console.warn(
+  //                 `buffer length = ${bufferLength}, content length = ${contentLength}`
+  //               );
+  //               proxyReq.setHeader("content-length", bufferLength);
+  //             }
+  //             proxyReq.write(bodyData);
+  //           }
+  //         }
+  //       }
+  //     );
 
-      proxyServer.on(
-        "proxyRes",
-        (
-          proxyRes: http.IncomingMessage,
-          req: express.Request,
-          res: express.Response
-        ) => {
-          res.status(proxyRes.statusCode ?? 500);
-          Object.entries(proxyRes.headers).forEach(([key, value]) => {
-            res.set(key, value as string | string[]);
-          });
+  //     proxyServer.on(
+  //       "proxyRes",
+  //       (
+  //         proxyRes: http.IncomingMessage,
+  //         req: express.Request,
+  //         res: express.Response
+  //       ) => {
+  //         res.status(proxyRes.statusCode ?? 500);
+  //         Object.entries(proxyRes.headers).forEach(([key, value]) => {
+  //           res.set(key, value as string | string[]);
+  //         });
 
-          let body = Buffer.alloc(0);
-          proxyRes.on("data", (data) => (body = Buffer.concat([body, data])));
-          proxyRes.on("end", () => res.end(body.toString()));
-          proxyRes.on("error", () =>
-            res.status(httpStatus.INTERNAL_SERVER_ERROR).end()
-          );
-        }
-      );
-    },
-  ],
+  //         let body = Buffer.alloc(0);
+  //         proxyRes.on("data", (data) => (body = Buffer.concat([body, data])));
+  //         proxyRes.on("end", () => res.end(body.toString()));
+  //         proxyRes.on("error", () =>
+  //           res.status(httpStatus.INTERNAL_SERVER_ERROR).end()
+  //         );
+  //       }
+  //     );
+  //   },
+  // ],
 });
